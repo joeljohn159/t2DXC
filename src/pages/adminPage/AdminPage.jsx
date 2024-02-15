e48c96c1e5e22def2bb1a4f83dacc29b459c476d
John <joel.john@dxc.com>
2024-02-15 10:24:54 -0500

    Changes in AdminPage

--- src/pages/adminPage/AdminPage.jsx
+++ src/pages/adminPage/AdminPage.jsx
@@ -3,7 +3,7 @@ import AdminHeader from "./components/AdminHeader.jsx";
 export default function UserPage(){
     return (
         <>
-        <h2>I am Main AdminPage!</h2>
+        <h2>I am Main AdminPage! New changes sdsd</h2>
         <AdminHeader />
         </>
     );
